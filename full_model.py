0a5e21f743aad49012221e7aaa8180d4c72cfc9a
beadeoliveira <beacdeoliveira@gmail.com>
2022-10-22 18:05:39 -0400

    adding comments for clarity

--- full_model.py
+++ full_model.py
@@ -7,70 +7,83 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
-# Path to the given CSV containing the data
-path_to_file = "/Users/beadeoliveira/Desktop/data.csv"
 
-# df = pd.read_csv(path_to_file, index_col='identifier').T
-df = pd.read_csv(path_to_file)
+# DESCRIPTION: This model will run a regression on all of the data over the
+# time period given, treating identifier, market cap, the factors, etc as
+# independent variables. The regression is completed using SKLearn which
+# utilizes test and training data to fit a learned model to the data.
 
-# setting the index of the graph as the date so that there is a regression
-# over the dates
-df.set_index(pd.DatetimeIndex(df['date']), inplace=True)
+def calculate_model(data):
+    # Sets the index of the graph as the date so that the regression occurs
+    # over the dates
+    data.set_index(pd.DatetimeIndex(data['date']), inplace=True)
 
-# df = df[df['identifier'] == 'PEOLTD6JT1H8']
+    # Sets the  predictor values
+    predictors = ['market_cap', 'sector', 'index_membership', 'factor_1',
+                  'factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6',
+                  'factor_7', 'factor_8', 'factor_9', 'factor_10']
 
-predictors = ['market_cap', 'sector', 'index_membership', 'factor_1',
-              'factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6',
-              'factor_7', 'factor_8', 'factor_9', 'factor_10']
+    # Uses the train_test_split to randomly select 30% of the data as testing
+    # data and saving the rest for the creation/training of the model
+    train, test = train_test_split(data, test_size=0.3)
 
-model = RandomForestClassifier(n_estimators=100, min_samples_split=200,
-                               random_state=1)
+    # Defines the model as a Linear Regression
+    model = LinearRegression()
 
-'''lab = preprocessing.LabelEncoder()
-df['target'] = lab.fit_transform(df['target'])'''
+    # Fits the model using the predictors above and defined target training data
+    model.fit(train[predictors], train["target"])
 
-print(df.head(5))
+    # Creates the predictors using the model.predict
+    preds = model.predict(test[predictors])
+    preds = pd.Series(preds, index=test.index)
 
-train, test = train_test_split(df, test_size=0.3)
+    # Calculates the r^2 score based on the test values and the predicted values
+    r = r2_score(test["target"], preds)
 
-model = LinearRegression()
-model.fit(train[predictors], train["target"])
+    combined = pd.concat({"target": test["target"], "Predictions": preds},
+                         axis=1)
+    combined.plot()
+    plt.title("All of them Together")
+    plt.show()
 
-preds = model.predict(test[predictors])
-preds = pd.Series(preds, index=test.index)
+    # k_fold test
+    # predictor (x) and response variables (y)
+    y = data['target']
+    X = data[['market_cap', 'sector', 'index_membership', 'factor_1',
+            'factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6',
+            'factor_7', 'factor_8', 'factor_9', 'factor_10']]
 
-r = r2_score(test["target"], preds)
+    # Conducts the K_Fold test
+    cv = KFold(n_splits=10, random_state=1, shuffle=True)
 
-print(r)
+    # modl = LinearRegression()
 
-combined = pd.concat({"target": test["target"], "Predictions": preds}, axis=1)
-combined.plot()
-plt.title("All of them Together")
-# plt.show()
+    # Calculates the cross validation score which is later used to calculate the
+    # RMSE below
+    scores = cross_val_score(model, X, y,
+                             scoring='neg_mean_absolute_error',
+                             cv=cv, n_jobs=-1)
 
-# Now do k_fold test
-# predictor (x) and response variables (y)
-y = df['target']
-X = df[['market_cap', 'sector', 'index_membership', 'factor_1',
-        'factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6',
-        'factor_7', 'factor_8', 'factor_9', 'factor_10']]
+    # the lower the RMSE the better
+    print("r^2 is = " + str(r))
+    print('root mean squared error (RMSE) = ' + str(np.sqrt(np.mean(np.absolute(
+        scores)))))
 
-cv = KFold(n_splits=10, random_state=1, shuffle=True)
-
-modl = LinearRegression()
-
-scores = cross_val_score(modl, X, y,
-                         scoring='neg_mean_absolute_error',
-                         cv=cv, n_jobs=-1)
-
-
-# the lower the RMSE the better
-print('root mean squared error (RMSE) = ' + str(np.sqrt(np.mean(np.absolute(
-    scores)))))
 
 if __name__ == '__main__':
-    print('done')
+
+    # Path to the given CSV containing the data
+    # TODO: insert the path to the dataset that you would like to analyze
+    # In this instance, I have used the path to the file on my computer as an
+    # example
+    path_to_file = "/Users/beadeoliveira/Desktop/data.csv"
+
+    # Reads the information contained in the CSV
+    df = pd.read_csv(path_to_file)
+
+    calculate_model(df)
+
+    print('\n Model Complete')
--- individual_models.py
+++ individual_models.py
@@ -2,10 +2,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import numpy as np
-from sklearn import preprocessing
-from sklearn import utils
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
@@ -13,85 +10,83 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
 
+# DESCRIPTION: This model will run a regression for individual idenfitiers
+# which are specified by the user. It functions similarly to the full model,
+# but is constrained to each specified identifier
+
+
+# Creates a function which take a list of identifiers and creates a model for
+# each
 def individual_idents(lst):
 
-    identifiers = list(set(df2['identifier'].tolist()))
-    ident = df2['identifier'].tolist()
+    for iden in lst:
 
-    '''fig, axis = plt.subplots(2, 2)
-    fig.suptitle("Predictive Models Based on Identifier", fontsize=8, y=0.95)'''
+        df = df2
+        df = df.loc[df['identifier'] == iden]
 
-    for iden in idents:
+        # Sets the index of the graph as the date so that the regression occurs
+        # over the dates
+        df.set_index(pd.DatetimeIndex(df['date']), inplace=True)
 
-        if (ident.count(iden)) > 20:
-            df = df2
-            df = df.loc[df['identifier'] == iden]
+        # Sets the  predictor values
+        predictors = ['market_cap',	'sector', 'index_membership', 'factor_1',
+                  'factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6',
+                  'factor_7', 'factor_8', 'factor_9', 'factor_10']
 
-            #setting the index of the graph as the date so that there is a regression
-            # over the dates
-            df.set_index(pd.DatetimeIndex(df['date']), inplace=True)
+        # Uses the train_test_split to randomly select 30% of the data as
+        # testing data and saving the rest for the creation/training of the
+        # model
+        train, test = train_test_split(df, test_size=0.3)
 
-            #print(df.head(10))
+        # Defines the model as a Linear Regression
+        model = LinearRegression()
 
-            #df.plot.line(y='target', x = 'date')
-            #plt.show()
+        # Fits the model using the predictors above and defined target
+        # training data
+        model.fit(train[predictors], train["target"])
 
-            predictors = ['market_cap',	'sector', 'index_membership', 'factor_1',
-                      'factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6',
-                      'factor_7', 'factor_8', 'factor_9', 'factor_10']
+        # Creates the predictors using the model.predict
+        preds = model.predict(test[predictors])
+        preds = pd.Series(preds, index=test.index)
 
-            model = RandomForestClassifier(n_estimators=100, min_samples_split=200, random_state=1)
+        # Calculates the r^2 score based on the test values and the predicted
+        # values
+        r = r2_score(test["target"], preds)
+        combined = pd.concat({"target": test["target"],"Predictions": preds}, axis=1)
+        combined.plot()
+        plt.title(iden + " with r^2: " + str(r))
 
-            #print(df.head(5))
+        print("r2 of " + iden + " =" + str(r))
 
-            train, test = train_test_split(df, test_size=0.3)
+        y = df['target']
+        X = df[['market_cap', 'sector', 'index_membership', 'factor_1',
+                'factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6',
+                'factor_7', 'factor_8', 'factor_9', 'factor_10']]
 
-            model = LinearRegression()
-            model.fit(train[predictors], train["target"])
+        cv = KFold(n_splits=10, random_state=1, shuffle=True)
 
-            preds = model.predict(test[predictors])
-            preds = pd.Series(preds, index=test.index)
+        modl = LinearRegression()
 
-            r = r2_score(test["target"], preds)
+        scores = cross_val_score(modl, X, y,
+                                 scoring='neg_mean_absolute_error',
+                                 cv=cv, n_jobs=-1)
 
-            combined = pd.concat({"target": test["target"],"Predictions": preds}, axis=1)
-            combined.plot()
-            plt.title(iden + " with r^2: " + str(r))
-            #a = ax[n][0]
-            #b = ax[n][1]
-            #axis[a, b].plot(combined)
-            #axis[a, b].set_title(iden)
-            #plt.title(iden)
-            #plt.plot()
-
-            print("r2 of " + iden + " =" + str(r))
-
-            y = df['target']
-            X = df[['market_cap', 'sector', 'index_membership', 'factor_1',
-                    'factor_2', 'factor_3', 'factor_4', 'factor_5', 'factor_6',
-                    'factor_7', 'factor_8', 'factor_9', 'factor_10']]
-
-            cv = KFold(n_splits=10, random_state=1, shuffle=True)
-
-            modl = LinearRegression()
-
-            scores = cross_val_score(modl, X, y,
-                                     scoring='neg_mean_absolute_error',
-                                     cv=cv, n_jobs=-1)
-
-            # the lower the RMSE the better
-            print('root mean squared error (RMSE) = ' + str(
-                np.sqrt(np.mean(np.absolute(
-                    scores)))))
+        # the lower the RMSE the better
+        print('root mean squared error (RMSE) = ' + str(
+            np.sqrt(np.mean(np.absolute(
+                scores)))))
 
     plt.show()
 
 if __name__ == '__main__':
 
     # Path to the given CSV containing the data
+    # TODO: insert the path to the dataset that you would like to analyze
+    # In this instance, I have used the path to the file on my computer as an
+    # example
     path_to_file = "/Users/beadeoliveira/Desktop/data.csv"
 
-    # df = pd.read_csv(path_to_file, index_col='identifier').T
+    # Reads the information contained in the CSV
     df2 = pd.read_csv(path_to_file)
 
     idents = []
